f9cee7900540edf72ed371e3bd595c28e6f7f607
Julian Munyard <julianmunyard@Julians-MacBook-Pro-2.local>
2025-09-13 15:44:41 +1000

    Initial commit for new label

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import './globals.css'
+
+export const metadata = {
+  title: 'JM x AMNISIA',
+  description: '',
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <head>
+        <link
+          rel="preload"
+          href="/fonts/Village.ttf"
+          as="font"
+          type="font/ttf"
+          crossOrigin="anonymous"
+        />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
